9eef656c43e5f61d9c7ca5d54b314362d9dd6f98
William Wang <williamblue543@gmail.com>
2022-11-24 00:03:16 -0500

    Initial Commit

---
+++ Rice Price/Rice_PriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rice_PriceApp.swift
+//  Rice Price
+//
+//  Created by William Wang on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Rice_PriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
